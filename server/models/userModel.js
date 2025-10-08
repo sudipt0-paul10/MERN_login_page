ce6ab5cc9b52854d31d2a74a9f025c83e6d8bfec
sudipt0-paul10 <sudipto.official10@gmail.com>
2025-10-08 10:42:08 +0530

    userModel.js commit full code

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,39 +1,38 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema({
-  name : String,
-  email : String,
+  name: String,
+  email: String,
   password: {
     type: String,
-    minLength: [8,"Password must have atleast 8 charcaters"],
-    maxLength: [32,"Password can't be greater than 32 characters"], 
+    minLength: [8, "Password must have at least 8 characters."],
+    maxLength: [32, "Password cannot have more than 32 characters."],
+    select: false,
   },
   phone: String,
-  accountVerified: {type: Boolean, default: false},
+  accountVerified: { type: Boolean, default: false },
   verificationCode: Number,
-  verificatonCodeExpire: Date,
+  verificationCodeExpire: Date,
   resetPasswordToken: String,
   resetPasswordExpire: Date,
   createdAt: {
-    type: Date, 
+    type: Date,
     default: Date.now,
   },
 });
 
-//pre is a mongoose middleware that runs before saving the document, e.g. hashing password before saving so that you don't have to hash manually every time 
-
-userSchema.pre("save", async function(next){
-  if(!this.isModified("password")){
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
     next();
   }
-  //hash(data,saltRounds) = saltRounds = numeber of times data is hashed/ strongth of hashing
   this.password = await bcrypt.hash(this.password, 10);
 });
 
-//this method compares the entered password with that of hashed passsword in db 
-userSchema.methods.comparePassword = async function(enteredPassword){
-  return await bcrypt.compare(enteredPassword,this.password);
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 userSchema.methods.generateVerificationCode = function () {
@@ -52,6 +51,23 @@ userSchema.methods.generateVerificationCode = function () {
   return verificationCode;
 };
 
+userSchema.methods.generateToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
+userSchema.methods.generateResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-export const User = mongoose.model("User",userSchema);
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+  return resetToken;
+};
+
+export const User = mongoose.model("User", userSchema);
