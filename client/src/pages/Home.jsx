2e1421db3056a60fe2b412789b4762f1c44a54db
sudipt0-paul10 <sudipto.official10@gmail.com>
2025-10-09 19:59:37 +0530

    pages commited full code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Hero from "../components/Hero";
 import Instructor from "../components/Instructor";
 import Technologies from "../components/Technologies";
@@ -10,7 +10,39 @@ import { Navigate, useNavigate } from "react-router-dom";
 import Footer from "../layout/Footer";
 
 const Home = () => {
-  return <></>;
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+
+  const logout = async () => {
+    await axios
+      .get("http://localhost:4000/api/v1/user/logout", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        toast.success(res.data.message);
+        setUser(null);
+        setIsAuthenticated(false);
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+        console.error(err);
+      });
+  };
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/auth"} />;
+  }
+
+  return (
+    <>
+      <section className="home">
+        <Hero />
+        <Instructor />
+        <Technologies />
+        <Footer />
+        <button onClick={logout}>Logout</button>
+      </section>
+    </>
+  );
 };
 
 export default Home;
