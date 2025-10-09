68ccd90b421821dfac433cf9b5b173ded837ca17
sudipt0-paul10 <sudipto.official10@gmail.com>
2025-10-09 19:57:46 +0530

    auth.jsx commited full code

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "../styles/Auth.css";
+import { Context } from "../main";
+import { Navigate } from "react-router-dom";
+import Register from "../components/Register";
+import Login from "../components/Login";
 
 const Auth = () => {
-  return <></>;
+  const { isAuthenticated } = useContext(Context);
+  const [isLogin, setIsLogin] = useState(true);
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <>
+      <div className="auth-page">
+        <div className="auth-container">
+          <div className="auth-toggle">
+            <button
+              className={`toggle-btn ${isLogin ? "active" : ""}`}
+              onClick={() => setIsLogin(true)}
+            >
+              Login
+            </button>
+            <button
+              className={`toggle-btn ${!isLogin ? "active" : ""}`}
+              onClick={() => setIsLogin(false)}
+            >
+              Register
+            </button>
+          </div>
+          {isLogin ? <Login /> : <Register />}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Auth;
